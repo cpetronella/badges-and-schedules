405e0fadf126c07d3ba8a5aea070876aa551f36a
Christopher <christopher.petronella@gmail.com>
2020-11-19 00:14:05 -0500

    commit tree a8649ca42197dd5b8b7508a54a144a5da4a3118f

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+def batch_badge_creator(array)
+  array_array = []
+  array.each do |name|
+    array_array.push("Hello, my name is #{name}.")
+  end
+  return array_array
+end
+def assign_rooms(array)
+  array_array = []
+  counter = 1
+  array.each do |name|
+    array_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return array_array
+end
+def printer(array)
+  batch_badge_creator(array).each do |badges|
+    puts badges
+  end 
+  assign_rooms(array).each do |badges|
+    puts badges
+  end
+end
+
+    
+
